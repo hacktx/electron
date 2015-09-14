c527df40f9d0f5cab439989b5af986621bb91c60
Thomas Gaubert <tpgaubert@gmail.com>
2015-09-13 21:22:13 -0500

    Manually feed frames to BarcodeDetector for better camera preview

--- app/src/main/java/com/hacktx/electron/activities/MainActivity.java
+++ app/src/main/java/com/hacktx/electron/activities/MainActivity.java
@@ -3,13 +3,22 @@ package com.hacktx.electron.activities;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,24 +27,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.hacktx.electron.R;
 import com.hacktx.electron.utils.PreferencesUtils;
 import com.hacktx.electron.vision.BarcodeTrackerFactory;
-import com.hacktx.electron.vision.CameraSourcePreview;
-import com.hacktx.electron.vision.GraphicOverlay;
 import com.hacktx.electron.vision.VisionCallback;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
-    private CameraSourcePreview mPreview;
-    private GraphicOverlay mGraphicOverlay;
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private BarcodeDetector barcodeDetector;
     private CameraSource mCameraSource;
-    private boolean scanning;
+    private boolean scanning, cameraRunning;
 
     @Override
     public void onCreate(Bundle state) {
@@ -52,16 +63,19 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(R.string.app_name);
         }
 
-        mPreview = (CameraSourcePreview) findViewById(R.id.preview);
-        mGraphicOverlay = (GraphicOverlay) findViewById(R.id.overlay);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
 
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
-        BarcodeTrackerFactory barcodeFactory = new BarcodeTrackerFactory(mGraphicOverlay, new VisionCallback() {
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        BarcodeTrackerFactory barcodeFactory = new BarcodeTrackerFactory(null, new VisionCallback() {
             @Override
             public void onFound(final Barcode barcode) {
                 runOnUiThread(new Runnable() {
                     public void run() {
-                        if(scanning && barcode.format == Barcode.QR_CODE) {
+                        if(scanning) {
                             scanning = false;
                             showConfirmationDialog(barcode.rawValue);
                         }
@@ -71,38 +85,31 @@ public class MainActivity extends AppCompatActivity {
         });
         barcodeDetector.setProcessor(new MultiProcessor.Builder<>(barcodeFactory).build());
 
-        mCameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedPreviewSize(1600, 1024)
-                .build();
-    }
-
-    private void startCameraSource() {
-        try {
-            mPreview.start(mCameraSource, mGraphicOverlay);
-            scanning = true;
-        } catch (IOException e) {
-            mCameraSource.release();
-            mCameraSource = null;
+        if(!barcodeDetector.isOperational()) {
+            Snackbar.make(findViewById(android.R.id.content), R.string.activity_main_detector_nonoperational, Snackbar.LENGTH_INDEFINITE).show();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        startCameraSource();
+        if(camera != null) {
+            camera.startPreview();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mPreview.stop();
+        if(camera != null) {
+            camera.stopPreview();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mCameraSource.release();
+        stopCamera();
     }
 
     @Override
@@ -192,5 +199,86 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         builder.show();
+    }
+
+    private void startCamera() {
+        if(cameraRunning) {
+            return;
+        }
+
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Snackbar.make(findViewById(android.R.id.content), R.string.activity_main_camera_failed, Snackbar.LENGTH_INDEFINITE).show();
+        }
+
+        if (camera != null) {
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                setCameraFrameCallback();
+                camera.startPreview();
+                cameraRunning = true;
+                scanning = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopCamera() {
+        if(camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+            cameraRunning = false;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if(camera != null) {
+            camera.setDisplayOrientation(90);
+            setCameraFrameCallback();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        startCamera();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        stopCamera();
+    }
+
+    private void setCameraFrameCallback() {
+        camera.setPreviewCallback(new Camera.PreviewCallback() {
+            @Override
+            public void onPreviewFrame(byte[] data, Camera camera) {
+                Camera.Parameters parameters = camera.getParameters();
+                int width = parameters.getPreviewSize().width;
+                int height = parameters.getPreviewSize().height;
+
+                YuvImage yuv = new YuvImage(data, parameters.getPreviewFormat(), width, height, null);
+
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                yuv.compressToJpeg(new Rect(0, 0, width, height), 50, out);
+
+                byte[] bytes = out.toByteArray();
+                final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                SparseArray<Barcode> barcodes = barcodeDetector.detect(new Frame.Builder().setBitmap(bitmap).build());
+                for(int i = 0; i < barcodes.size(); i++) {
+                    Barcode barcode = barcodes.get(barcodes.keyAt(i));
+                    System.out.println("BARCODE!: " + barcode.rawValue);
+                    if(scanning) {
+                        scanning = false;
+                        System.out.println("DIALOG");
+                        showConfirmationDialog(barcode.rawValue);
+                    }
+                }
+            }
+        });
     }
 }
